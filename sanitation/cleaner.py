08889feb55c81c9485a28f776e4e37e226420a0f
MarzukIngi <mil4@hi.is>
2021-11-09 19:33:49 +0000

    cleanex

--- sanitation/cleaner.py
+++ sanitation/cleaner.py
@@ -1,107 +1,37 @@
-from __future__ import unicode_literals, print_function, division
 from io import open
-import unicodedata
-import string
-import re
-import random
+from datasets import Dataset
+from datasets import DatasetDict
 
-import torch
-import torch.nn as nn
-from torch import optim
-import torch.nn.functional as F
+lang1 = "de"
+lang2 = "is"
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-SOS_token = 0
-EOS_token = 1
-
-
-class Lang:
-    def __init__(self, name):
-        self.name = name
-        self.word2index = {}
-        self.word2count = {}
-        self.index2word = {0: "SOS", 1: "EOS"}
-        self.n_words = 2  # Count SOS and EOS
-
-    def addSentence(self, sentence):
-        for word in sentence.split(' '):
-            self.addWord(word)
-
-    def addWord(self, word):
-        if word not in self.word2index:
-            self.word2index[word] = self.n_words
-            self.word2count[word] = 1
-            self.index2word[self.n_words] = word
-            self.n_words += 1
-        else:
-            self.word2count[word] += 1
-
-# Lowercase, trim, and remove non-letter characters
-
-
-def normalizeString(s):
-    s = s.lower().strip()
-    s = re.sub(r"([.!?])", r" \1", s)
-    s = re.sub(r"[^a-zA-Z.!?ÁÐÉÍÓÚÝÞÆÖ]+", r" ", s)
-    return s
-
-def readLangs(lang1, lang2, reverse=False):
-    print("Reading lines...")
-
-    # Read the file and split into lines
-    lines = open('data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
+lines = open('%s-%s.bitextf.tsv' % (lang1, lang2), encoding='utf-8').\
         read().strip().split('\n')
 
-    # Split every line into pairs and normalize
-    pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+lines2 = open('Sentence pairs in Icelandic-German - 2021-11-08.tsv', encoding='utf-8').\
+         read().strip().split('\n')
 
-    # Reverse pairs, make Lang instances
-    if reverse:
-        pairs = [list(reversed(p)) for p in pairs]
-        input_lang = Lang(lang2)
-        output_lang = Lang(lang1)
-    else:
-        input_lang = Lang(lang1)
-        output_lang = Lang(lang2)
+tatoebaDict = { 'id': [], 'translation': [] }
 
-    return input_lang, output_lang, pairs
+for i in range(len(lines2)):
+    lines2[i] = lines2[i].split('\t')
+    tatoebaDict['id'].append(i)
+    tatoebaDict['translation'].append({ 'de': lines2[i][3], 'is': lines2[i][1]})
+    
+ccmatrixDict = { 'id': [], 'translation': [] }
 
-MAX_LENGTH = 10
+for i in range(len(lines)):
+    lines[i] = lines[i].split('\t')
+    ccmatrixDict['id'].append(i)
+    ccmatrixDict['translation'].append({ 'de': lines[i][2], 'is': lines[i][1] })
 
-eng_prefixes = (
-    "i am ", "i m ",
-    "he is", "he s ",
-    "she is", "she s ",
-    "you are", "you re ",
-    "we are", "we re ",
-    "they are", "they re "
-)
+tatoebaDataset = Dataset.from_dict(tatoebaDict)
 
+ccmatrixDataset = Dataset.from_dict(ccmatrixDict)
 
-def filterPair(p):
-    return len(p[0].split(' ')) < MAX_LENGTH and \
-        len(p[1].split(' ')) < MAX_LENGTH and \
-        p[1].startswith(eng_prefixes)
+tatoebaData = DatasetDict({'train': tatoebaDataset})
 
+ccmatrixData = DatasetDict({'train': ccmatrixDataset})
 
-def filterPairs(pairs):
-    return [pair for pair in pairs if filterPair(pair)]
-
-def prepareData(lang1, lang2, reverse=False):
-    input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
-    print("Read %s sentence pairs" % len(pairs))
-    pairs = filterPairs(pairs)
-    print("Trimmed to %s sentence pairs" % len(pairs))
-    print("Counting words...")
-    for pair in pairs:
-        input_lang.addSentence(pair[0])
-        output_lang.addSentence(pair[1])
-    print("Counted words:")
-    print(input_lang.name, input_lang.n_words)
-    print(output_lang.name, output_lang.n_words)
-    return input_lang, output_lang, pairs
-
-
-input_lang, output_lang, pairs = prepareData('eng', 'fra', True)
-print(random.choice(pairs))
+print(tatoebaData)
+print(ccmatrixData)
